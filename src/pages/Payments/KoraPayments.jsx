050b48a4d93375a778848a42c459abbdd9ad85e1
kibetkorir395 <kibetkorir395@gmail.com>
2026-03-25 03:25:53 +0300

    Update KoraPayments.jsx

--- src/pages/Payments/KoraPayments.jsx
+++ src/pages/Payments/KoraPayments.jsx
@@ -54,7 +54,7 @@ export default function KoraPayments({ setUserData }) {
         key: "pk_live_Gu3aUUGAzWj1zeonHdwBAi4oDD9Vc4AViyHWqALp",
         reference: `ref-${Date.now()}`,
         amount: price,
-        currency: "GHS",
+        currency: "KES",
         customer: {
             name: currentUser.email,
             email: currentUser.email,
